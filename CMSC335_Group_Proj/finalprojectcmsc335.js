2c61512a001e4159b1bd12cc5a996b973560c896
Dryzon13 <jjbonya@gmail.com>
2023-05-15 21:45:37 -0400

    mongo stuff

--- CMSC335_Group_Proj/finalprojectcmsc335.js
+++ CMSC335_Group_Proj/finalprojectcmsc335.js
@@ -80,3 +80,18 @@ process.stdin.on("readable", function () {
       process.stdin.resume();
     }
 });
+
+// Database Functions
+
+require("dotenv").config({ path: path.resolve(__dirname, 'credentials/.env') })  
+
+const userName = process.env.MONGO_DB_USERNAME;
+const password = process.env.MONGO_DB_PASSWORD;
+
+/* Our database and collection */
+const databaseAndCollection = {db: process.env.MONGO_DB_NAME, collection:process.env.MONGO_COLLECTION};
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = `mongodb+srv://${userName}:${password}@cluster0.zho1gsz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
