cb4c8ca6693b89e70c72de6b215589c71647ec59
Dryzon13 <jjbonya@gmail.com>
2023-05-16 21:48:53 -0400

    fixed mongo

--- CMSC335_Group_Proj/finalprojectcmsc335.js
+++ CMSC335_Group_Proj/finalprojectcmsc335.js
@@ -6,8 +6,6 @@ const app = express(); /* app is a request handler function */
 app.set("views", path.resolve(__dirname, "templates"));
 app.set("view engine", "ejs");
 
-const portNumber = process.argv[2];
-
 
 require("dotenv").config({ path: path.resolve(__dirname, 'credentials/.env') })  
 
@@ -33,56 +31,57 @@ app.get("/", (request, response) => {
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.post("/processApplication", (request, response) => {
+app.post("/processApplication", async (request, response) => {
   const { name, email, gpa, year, type, reason, comment } =  request.body;
   const v = { name: name, email: email, gpa: gpa, year: year, type: type, reason:reason, comment:comment };
-  processInsert(v);
-  process();
+  await processInsert(v);
+  const students = await processTable();
+  var htmlcode = "<table border='1'> </tr> <th>Name</th> <th>Year</th>  </tr> ";
+    students.forEach(element => {
 
+      htmlcode += '<tr>' + '<td> ' + element.name + ' </td> <td> ' + element.year + '</td> </tr>';
+    });
 
-  async function processInsert(v) {
+    htmlcode += " </table>";
+    const variables = { name: name, email: email, gpa: gpa, year: year, type: type, reason:reason, comment:comment, table: htmlcode };
+    response.render("confirmation", variables);
+}); 
 
-    try {
-        await client.connect();
-        await insertData(client, databaseAndCollection, v);
+async function processInsert(v) {
+  try {
+      await client.connect();
+      const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(v);
 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
   }
+}
 
 async function insertData(client, databaseAndCollection, applicant) {
     const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(applicant);
 }
 
 
-async function process() {
+async function processTable() {
+  results = null;
+  try {
+    await client.connect();
+    let filter = {};
+    const cursor = client.db(databaseAndCollection.db)
+        .collection(databaseAndCollection.collection).find(filter);
+        
+        /* Turn this into a table to add as html code into the final page*/
+    result = await cursor.toArray();
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+    return result;
+  }
+}
 
-try {
-  await client.connect();
-  let filter = {};
-  const cursor = client.db(databaseAndCollection.db)
-      .collection(databaseAndCollection.collection).find(filter);
-      
-      /* Turn this into a table to add as html code into the final page*/
-  const result = await cursor.toArray();
-  var htmlcode = "<table border='1'> </tr> <th>Name</th> <th>Year</th>  </tr> ";
-  result.forEach(element => {
-
-    htmlcode += '<tr>' + '<td> ' + element.name + ' </td> <td> ' + element.year + '</td> </tr>';
-  });
-
-  htmlcode += " </table>";
-  const variables = { table: htmlcode };
-  response.render("confirmation", variables);
-} catch (e) {
-  console.error(e);
-} finally {
-  await client.close();
-}}
-}); 
 
 
 
@@ -93,7 +92,36 @@ if (process.argv.length != 3) {
   process.exit(1);
 }
 
+const filename = process.argv[2];
+
+const prompt = "Type stop to shutdown the server: ";
+process.stdout.write(prompt);
+
+process.stdin.on("readable", function () {
+  let dataInput = process.stdin.read();
+  let command = dataInput.trim();
+  if (command === "stop") {
+      console.log("Shutting down the server");
+      process.exit(0);  /* exiting */
+    } else {
+      console.log(`Invalid command: ${command}`);
+      process.stdout.write(prompt);
+      process.stdin.resume();
+    }
+});
+
+const portNumber = process.argv[2];
+
 app.listen(portNumber);
+console.log(`Web server started and running at http://localhost:${portNumber}`);
+
+
+/*if (process.argv.length != 3) {
+  process.stdout.write(`Usage ${process.argv[1]} port Number`);
+  process.exit(1);
+}*/
+
+//app.listen(portNumber);
 console.log(`Web server started and running at https://finalprojectcmsc335.onrender.com`);
 
 
